8b27cdffcb6197d392c7f881b782d838479353b0
Anamay-Agnihotri <31450471+Anamay-Agnihotri@users.noreply.github.com>
2018-03-03 21:28:09 -0500

    Add files via upload

---
+++ dictionrbt.h
@@ -0,0 +1,40 @@
+//====================================================================
+// Anamay Agnihotri and Aixin Li
+// Submitted: November 17, 2017
+// dictionrbt.cpp
+// Dictionary definition for RBT
+//====================================================================
+
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+#include "redblack.h"
+
+#ifndef DICTIONRBT_H
+#define DICTIONRBT_H
+
+
+
+template<class KeyType>
+class dictionrbt : public RedBlack<KeyType>
+{
+	public:
+		/*constructor, destructor and copy constructor
+		inherited from BST<KeyType>*/
+		//using RedBlack<KeyType>::root;
+  //  using RedBlack<KeyType>:: NIL;
+
+    using RedBlack<KeyType>::empty;
+    using RedBlack<KeyType>::get;
+    using RedBlack<KeyType>::remove;
+    using RedBlack<KeyType>::insert;
+		using RedBlack<KeyType>::inOrder;
+
+};
+
+
+
+
+#endif
